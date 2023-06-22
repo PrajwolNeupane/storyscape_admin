2b0fb1e112440ca16e0b027b9b4058929e6489c5
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-06-23 00:00:43 +0545

    Dashboard UI Done

--- src/page/Dashboard.tsx
+++ src/page/Dashboard.tsx
@@ -1,14 +1,59 @@
-import { Avatar, Button, HStack, Heading, VStack, Text, Box } from "@chakra-ui/react";
+import { Avatar, Button, HStack, Heading, VStack, Text, Box, Table, Td, Th, Tr } from "@chakra-ui/react";
 import { FC } from "react";
+import { Link } from "react-router-dom";
 
 interface Props {
 
 }
 
 let Dashboard: FC<Props> = ({ }) => {
+
+    const data = [
+        {
+            id: 1,
+            date: 20230204,
+            title: "Hello",
+            link: "https://robohash.org/Prajwol%20Neupane0",
+            likes: 23,
+            dislikes: 2,
+        },
+        {
+            id: 2,
+            date: 20230205,
+            title: "Hello",
+            link: "https://robohash.org/Prajwol%20Neupane0",
+            likes: 23,
+            dislikes: 2,
+        },
+        {
+            id: 3,
+            date: 20230206,
+            title: "Hello",
+            link: "https://robohash.org/Prajwol%20Neupane0",
+            likes: 23,
+            dislikes: 2,
+        },
+        {
+            id: 4,
+            date: 20230207,
+            title: "Hello",
+            link: "https://robohash.org/Prajwol%20Neupane0",
+            likes: 23,
+            dislikes: 2,
+        },
+        {
+            id: 5,
+            date: 20230208,
+            title: "Hello",
+            link: "https://robohash.org/Prajwol%20Neupane0",
+            likes: 23,
+            dislikes: 2,
+        }
+    ]
+
     return (
         <VStack bgColor={'dark.600'} p={"40px 5%"} alignItems={"flex-start"} gap={"40px"}>
-            <HStack gap={"20px"} justifyContent={"flex-start"}>
+            <HStack gap={"60px"} justifyContent={"flex-start"}>
                 <Avatar src="https://robohash.org/Prajwol%20Neupane0" bgColor={"text.200"} width={"180px"} height={"180px"} />
                 <VStack alignItems={"start"}>
                     <Heading fontWeight={"semibold"} fontSize={"sm"} color={"text.300"}>Prajwol Neupane</Heading>
@@ -32,6 +77,65 @@ let Dashboard: FC<Props> = ({ }) => {
                     <Heading fontWeight={"bold"} fontSize={"xxxxl"} color={"text.300"}>20</Heading>
                 </VStack>
             </HStack>
+            <Table variant={"simple"} color={"text.300"}>
+                <Tr >
+                    <Th color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        No.
+                    </Th>
+                    <Th w={"20%"} color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Published Date
+                    </Th>
+                    <Th w={"20%"} color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Title
+                    </Th>
+                    <Th color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Link
+                    </Th>
+                    <Th color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Total Likes
+                    </Th>
+                    <Th color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Total Dislikes
+                    </Th>
+                    <Th w={"20%"} color={"text.300"} textTransform={"none"} fontSize={"xs"} fontWeight={"semibold"}>
+                        Action
+                    </Th>
+                </Tr>
+                {
+                    data.map((curr, indx) => (
+                        <Tr key={indx}>
+                            <Td>
+                                {curr?.id}
+                            </Td>
+                            <Td>
+                                {curr?.date}
+                            </Td>
+                            <Td>
+                                {curr?.title}
+                            </Td>
+                            <Td>
+                                <Button as={Link} target="blank" to={curr?.link} p={"0px 20px"} fontWeight={"medium"} fontSize={"xs"} bgColor={"text.300"} _hover={{ bgColor: "text.200" }}>
+                                    View Blog
+                                </Button>
+                            </Td>
+                            <Td>
+                                {curr?.likes}
+                            </Td>
+                            <Td>
+                                {curr?.dislikes}
+                            </Td>
+                            <Td as={HStack}>
+                                <Button p={"0px 20px"} fontWeight={"medium"} fontSize={"xs"} bgColor={"text.300"} _hover={{ bgColor: "text.200" }}>
+                                    Update Blog
+                                </Button>
+                                <Button p={"0px 20px"} fontWeight={"medium"} fontSize={"xs"} bgColor={"error.500"} _hover={{ bgColor: "error.700" }} color={"dark.900"}>
+                                    Delete Blog
+                                </Button>
+                            </Td>
+                        </Tr>
+                    ))
+                }
+            </Table>
         </VStack>
     )
 }
