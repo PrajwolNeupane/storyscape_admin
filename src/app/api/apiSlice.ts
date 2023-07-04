9e0814d2321ad28144604a6cd93386e7af192a65
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-04 21:50:41 +0545

    Fixed URL Issues

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${{import.meta.env.VITE_REACT_API_URL}/api/v2`,
+  baseUrl: `${import.meta.env.VITE_REACT_API_URL}"/api/v2"`,
   prepareHeaders: (headers) => {
-    headers.set("api_key", `mero-54321-app`);
+    headers.set("api_key", `${import.meta.env.VITE_REACT_API_KEY}`);
     headers.set("Content-Type", "application/json");
     return headers;
   },
