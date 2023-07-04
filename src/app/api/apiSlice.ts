26a0341dbdfd43ecf2aae2b9d7a3a44f0b22b3d0
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-04 21:53:51 +0545

    Fixed Api Issues

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: `${import.meta.env.VITE_REACT_API_URL}"/api/v2"`,
+  baseUrl: `${import.meta.env.VITE_REACT_API_URL}/api/v2`,
   prepareHeaders: (headers) => {
     headers.set("api_key", `${import.meta.env.VITE_REACT_API_KEY}`);
     headers.set("Content-Type", "application/json");
