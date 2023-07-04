520389bf6919f039e1022e33b0fe3e78bfba1824
Prajwol <96991442+PrajwolNeupane@users.noreply.github.com>
2023-07-04 21:43:58 +0545

    Fixed URL

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8000/api/v2/",
+  baseUrl: `${{import.meta.env.VITE_REACT_API_URL}`,
   prepareHeaders: (headers) => {
     headers.set("api_key", `mero-54321-app`);
     headers.set("Content-Type", "application/json");
